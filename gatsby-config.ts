f149afe969f30a6ce523d61bc6c4134292bbd5e8
Nanok24 <ionmihneaandrei@gmail.com>
2023-05-11 16:13:30 +0300

    Updated dependencies for Netlify deployment

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,14 +1,19 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
+
+module.exports = {
+  plugins: ["gatsby-plugin-netlify"],
+};
 
 const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
+
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [],
-}
+};
 
-export default config
+export default config;
